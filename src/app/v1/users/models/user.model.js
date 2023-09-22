83cc66da3172399a2a4b787e4c613e658da7a90d
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-09-21 19:29:56 -0700

    Adding a sprinkle of stardust ✨

--- src/app/v1/users/models/user.model.js
+++ src/app/v1/users/models/user.model.js
@@ -9,45 +9,29 @@ const UsersSchema = new mongoose.Schema({
   },
   name: {
     type: String,
-    unique: true,
-    trim: true,
   },
   email: {
     type: String,
-    unique: true,
-    trim: true,
-    lowercase: true,
   },
   password: {
     type: String,
-    minlength: 5,
   },
   gender: {
     type: String,
-    trim: true,
   },
   age: {
     type: Number,
-    default: 0,
-    min: 0,
   },
   picture: {
     type: String,
-    trim: true,
   },
   address: {
     city: {
       type: String,
-      trim: true,
     },
     country: {
       type: String,
-      trim: true,
     },
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
   },
 });
 
