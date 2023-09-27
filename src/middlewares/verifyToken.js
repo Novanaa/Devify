1c28ed413f26d9752dfb34ab0645d06211819a74
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-09-26 18:30:57 -0700

    features/auth - new feat: Adding JsonWebToken verify tokens

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -1,6 +1,6 @@
-// import jwt from "jsonwebtoken";
-import validator from "validator";
+import jwt from "jsonwebtoken";
 import Response from "../utils/res.js";
+import { JWTACCESSTOKEN } from "../const/config.js";
 import createLogger from "../utils/logger.js";
 const logger = createLogger();
 const response = new Response();
@@ -8,13 +8,15 @@ const response = new Response();
 function verifyToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   try {
-    const token = authHeader.split(" ")[1];
-    if (!validator.isJWT(token)) return response.badRequest(res);
-    console.log(token);
-    next();
+    const token = authHeader && authHeader.split(" ")[1];
+    if (token == undefined) return response.unauth(res);
+    jwt.verify(token, JWTACCESSTOKEN, (err, decoded) => {
+      if (err) return response.unauth(res);
+      req.decode = decoded;
+      next();
+    });
   } catch (err) {
     logger.error(err);
-    next();
   }
 }
 
