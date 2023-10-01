fd98b05a528f3de1a8b5f208bc954e11b872193c
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-10-01 02:08:05 -0700

    features/carts - new feat: GET carts routes

--- src/app/v1/carts/routes/cart.routes.js
+++ src/app/v1/carts/routes/cart.routes.js
@@ -1,5 +1,9 @@
 import express from "express";
 import carts from "../controllers/cart.controller.js";
+import verifyToken from "../../../../middlewares/verifyToken.js";
 const router = express.Router();
+
+router.get("/", carts.getAllCarts);
+router.get("/:id", verifyToken, carts.getSingleCartsById);
 
 export default router;
