7997190ff7da5e8cb8e5685a480c9e8ac2e3b2fb
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-10-06 18:53:30 -0700

    features/carts - new feat: POST/Add carts

--- src/app/v1/carts/controllers/cart.controller.js
+++ src/app/v1/carts/controllers/cart.controller.js
@@ -1,5 +1,6 @@
 import { getAllCarts, getSingleCartsById } from "./cart.get.js";
+import addCarts from "./cart.post.js";
 
-const carts = { getAllCarts, getSingleCartsById };
+const carts = { getAllCarts, getSingleCartsById, addCarts };
 
 export default carts;
