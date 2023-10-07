933abd44a25dfb9245bdb681e4d2f645619c588a
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-10-07 00:55:24 -0700

    features/carts - new feat: PATCH/Update carts

--- src/app/v1/carts/controllers/cart.controller.js
+++ src/app/v1/carts/controllers/cart.controller.js
@@ -1,11 +1,14 @@
 import { getAllCarts, getSingleCartsById } from "./cart.get.js";
 import addCarts from "./cart.post.js";
 import { deleteCartById, deleteCartByUniquekey } from "./cart.delete.js";
+import { updateCartById, updateCartByUniquekey } from "./cart.patch.js";
 
 const carts = {
   getAllCarts,
   getSingleCartsById,
   addCarts,
+  updateCartById,
+  updateCartByUniquekey,
   deleteCartById,
   deleteCartByUniquekey,
 };
