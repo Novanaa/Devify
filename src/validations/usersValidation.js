561aee5332f2ea06480f64eef357c10cef68272e
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-10-07 21:48:20 -0700

    fix/user-validations - fix min age

--- src/validations/usersValidation.js
+++ src/validations/usersValidation.js
@@ -4,7 +4,11 @@ const usersValidation = Joi.object({
   name: Joi.string().max(15).required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(7).required(),
+<<<<<<< HEAD
   age: Joi.number().min(3),
+=======
+  age: Joi.number().min(12),
+>>>>>>> c260240d42322e7052668a116485c3331b82fe55
   gender: Joi.string().max(100),
   picture: Joi.string().max(200),
   image: Joi.string().optional(),
