7c3b95cf1ef5ad4bae4a76a574d9fb4124d75f41
ItsNvaa <kadeknovacahayaputra@gmail.com>
2023-10-26 02:15:51 -0700

    fix/default validation value

--- src/validations/productsValidation.js
+++ src/validations/productsValidation.js
@@ -6,8 +6,8 @@ const productsValidation = Joi.object({
   price: Joi.number().required().default(5),
   category: Joi.string().required().max(100),
   location: Joi.string().max(100),
-  rating: Joi.number().default(4),
-  stock: Joi.number().default(10),
+  rating: Joi.number().default(0),
+  stock: Joi.number().default(1),
   image: Joi.string(),
 });
 
